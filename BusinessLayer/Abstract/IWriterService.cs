465655acc6021d348e7d36c0dea4bc2bcb45d8c6
Muhammed Soysal <soysalmuhammed000@gmail.com>
2022-02-04 18:49:42 +0300

    40.Ders

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetList();
+        Writer WriterAdd(Writer writer);
+        Writer WriterDelete(Writer writer);
+        Writer WriterUpdate(Writer writer);
+        Writer GetByID(int id); 
+    }
+}
---
+++ BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        public Writer GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Writer WriterAdd(Writer writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Writer WriterDelete(Writer writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Writer WriterUpdate(Writer writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
